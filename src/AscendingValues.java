13e8955709c27b6097c14f2958bfdc31c792ef85
Gale <Gale@Galeto>
2017-02-25 20:06:51 +0200

    Ready1

--- src/AscendingValues.java
+++ src/AscendingValues.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class AscendingValues {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		
 		Scanner input = new Scanner(System.in);
